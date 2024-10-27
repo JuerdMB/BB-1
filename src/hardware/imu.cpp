d9319828974c0126deabbe671b8bee029afbe530
JuerdMB <j.q.mispelblombeijer@student.tudelft.nl>
2024-10-27 20:13:34 +0100

    initial unit tests

--- src/hardware/imu.cpp
+++ src/hardware/imu.cpp
@@ -8,6 +8,15 @@
 
 IMU::IMU() : icm_(Adafruit_ICM20948()), currentOrientation_(Orientation()), lastOrientationUpdate_(0)
 {
+    latestIMUData_.accelerometer.x = 0.;
+    latestIMUData_.accelerometer.y = 0.;
+    latestIMUData_.accelerometer.z = 0.;
+    latestIMUData_.gyroscope.x = 0.;
+    latestIMUData_.gyroscope.y = 0.;
+    latestIMUData_.gyroscope.z = 0.;
+    latestIMUData_.magnetometer.x = 0.;
+    latestIMUData_.magnetometer.y = 0.;
+    latestIMUData_.magnetometer.z = 0.;
 }
 
 bool IMU::init()
@@ -46,24 +55,30 @@ bool IMU::init()
     // Initialize accelerometer settings
     icm_.setAccelRange(ICM20948_ACCEL_RANGE_16_G);
     Logger::info("IMU - Accelerometer range set to: %d", icm_.getAccelRange());
-    return true;
 
     // Set up interrupt on IMU_INT_PIN
     pinMode(ICM_INTERRUPT, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(ICM_INTERRUPT), imuInterruptHandler, RISING);
     Logger::info("IMU - IMU interrupt set up on pin %d", ICM_INTERRUPT);
+
+    return true;
 }
 
 void IMU::retrieveRawData()
 {
     // Temporary store reading values
-    sensors_event_t *accel;
-    sensors_event_t *gyro;
-    sensors_event_t *temp;
-    sensors_event_t *mag;
+    sensors_event_t accel_event, gyro_event, temp_event, mag_event;
+    sensors_event_t *accel = &accel_event;
+    sensors_event_t *gyro = &gyro_event;
+    sensors_event_t *temp = &temp_event;
+    sensors_event_t *mag = &mag_event;
 
     // Update central accel, gyro, temperature and magnetometer values
-    icm_.getEvent(accel, gyro, temp, mag);
+    if (!icm_.getEvent(accel, gyro, temp, mag))
+    {
+        Logger::warn("retrieving data from ICM unsuccesful with [accel], [gyro], [temp], [mag]");
+        return;
+    }
 
     // Update central accel, gyro, temperature and magnetometer values
     latestIMUData_.accelerometer.x = accel->acceleration.x;
@@ -75,6 +90,11 @@ void IMU::retrieveRawData()
     latestIMUData_.magnetometer.x = mag->magnetic.x;
     latestIMUData_.magnetometer.y = mag->magnetic.y;
     latestIMUData_.magnetometer.z = mag->magnetic.z;
+    Logger::debug("Got new accelerometer, gyro and magnetometer values: [ %0.2f , %0.2f, %0.2f ] , [ %0.2f , %0.2f , %0.2f ] , [ %0.2f , %0.2f , %0.2f ]", 
+                                    latestIMUData_.accelerometer.x, latestIMUData_.accelerometer.y, latestIMUData_.accelerometer.z,
+                                    latestIMUData_.gyroscope.x, latestIMUData_.gyroscope.y, latestIMUData_.gyroscope.z,
+                                    latestIMUData_.magnetometer.x, latestIMUData_.magnetometer.y, latestIMUData_.magnetometer.z);
+
 }
 
 void IMU::updateFilteredOrientation()
